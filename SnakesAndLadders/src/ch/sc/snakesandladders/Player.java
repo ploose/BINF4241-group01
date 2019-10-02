fc240bee9834d17429a64f86f686f11f29878a78
Patrick <patrick.looser@uzh.ch>
2019-10-02 17:35:28 +0200

    Updated Players and Player class, added doc of classes and their function

---
+++ SnakesAndLadders/src/ch/sc/snakesandladders/Player.java
@@ -0,0 +1,27 @@
+// Patrick Looser
+
+/*
+getCurrentSquare:
+    return Square
+
+setCurrentSquare:
+    sets the current square
+ */
+
+package ch.sc.snakesandladders;
+public class Player {
+
+    String Name;
+    private Square  CurrentSquare;
+
+    public Square getCurrentSquare() {
+        return CurrentSquare;
+    }
+
+    public void setCurrentSquare(Square currentSquare) {
+        CurrentSquare = currentSquare;
+    }
+
+    public void moveFwd()
+
+}
--- SnakesAndLadders/src/ch/sc/snakesandladders/Players.java
+++ SnakesAndLadders/src/ch/sc/snakesandladders/Players.java
@@ -1,7 +1,25 @@
+// Created by Patrick Looser
+
 package ch.sc.snakesandladders;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Players {
 
-    
+    private Queue<String> playerQueue = new LinkedList<>();
+
+    public void setQueue(List inputList) {
+
+        this.playerQueue.addAll(inputList);
+
+    }
+
+    public List getQueue(){
+
+        return (List) this.playerQueue;
+    }
 
 }
