0a3a110fb87131ee0bebf71cda4516e05f81361d
Wassermalone <pascal.marty98@gmail.com>
2019-10-02 20:29:00 +0200

    Implemented LastSquare Class / pascal.marty

---
+++ SnakesAndLadders/src/ch/sc/snakesandladders/LastSquare.java
@@ -0,0 +1,20 @@
+//Author: Pascal Marty
+//TODO: Can multiple people land on the last square or is the game finished, as soon as the first player reaches it?
+public class LastSquare extends Square{
+    public LastSquare(int index){
+        super(index);
+    }
+
+    public boolean requestLanding(Player p){
+        // Assuming the game ends after first player reaches the last square, we don't need to check whether it is
+        // occupied, because there will always only be one player that reaches it.
+        addPlayer(p);
+        finishGame();
+        return true;
+    }
+
+    private void finishGame(){
+        /* Do stuff to finish the game */
+    }
+
+}
