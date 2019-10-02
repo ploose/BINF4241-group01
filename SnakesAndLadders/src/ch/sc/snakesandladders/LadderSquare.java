3c836c3f13dc625875fad8bc967d4391104ade14
Wassermalone <pascal.marty98@gmail.com>
2019-10-02 22:15:58 +0200

    Implemented LadderSquare. / pascal.marty

---
+++ SnakesAndLadders/src/ch/sc/snakesandladders/LadderSquare.java
@@ -0,0 +1,25 @@
+//Author: Pascal Marty
+import java.util.Random;
+
+public class LadderSquare extends Square{
+    private int jumpDistance;
+    public LadderSquare(Game game, int index){
+        super(game, index);
+        initDistance(this.index);
+    }
+
+    // Initializes distance, which the ladder will move the player, calculated between (index+1) and (end-1)
+    // TODO: Multiple ladders could end up at same square, also snakes could spawn on ladders
+    private initJumpDistance(int index){
+        Random random = new Random();
+        this.jumpDistance = random.nextInt(this.game.getSize() - index -1) + 1 + index;
+    }
+
+    public Square requestLanding(){
+        return moveAndLand(jumpDistance);
+    }
+
+    public boolean isLastSquare(){
+        return false;
+    }
+}
