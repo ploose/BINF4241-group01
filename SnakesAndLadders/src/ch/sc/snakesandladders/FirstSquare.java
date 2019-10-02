200951dacfed648498b68f6439b089b2fd095518
Wassermalone <pascal.marty98@gmail.com>
2019-10-02 20:21:57 +0200

    Implemented FirstSquare Class / pascal.marty

---
+++ SnakesAndLadders/src/ch/sc/snakesandladders/FirstSquare.java
@@ -0,0 +1,13 @@
+//Author: Pascal Marty
+public class FirstSquare extends Square{
+    public FirstSquare(int index){
+        // passes singleSpace = false because there can be multiple players on the first square
+        super(index, false);
+    }
+
+    public boolean requestLanding(Player p){
+        // Players can always land on the first square
+        addPlayer(p);
+        return true;
+    }
+}
