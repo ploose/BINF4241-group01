ba0548f82bf4af4bb6365fc4b4ab877dbc355e03
Tannebaum007 <45392509+Tannebaum007@users.noreply.github.com>
2019-10-02 16:10:11 +0200

    Created class Game /tim.brunner

---
+++ SnakesAndLadders/src/ch/sc/snakesandladders/Game.java
@@ -0,0 +1,13 @@
+public class Game {
+    private boolean is_running;
+    private Players players = new Players;
+    private Board board = new Board;
+
+    public static void main(String[] args) {
+
+    }
+
+    private Game() {}
+
+
+}
