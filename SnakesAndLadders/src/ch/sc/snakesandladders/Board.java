37c7b6b77c851c4c16ec24ca774ee39aa02217e5
Patrick Looser <palooser.rng@gmail.com>
2019-10-05 13:45:27 +0200

    Created a method to fill the board with squares correctly

--- SnakesAndLadders/src/ch/sc/snakesandladders/Board.java
+++ SnakesAndLadders/src/ch/sc/snakesandladders/Board.java
@@ -2,7 +2,10 @@ package ch.sc.snakesandladders;
 
 import ch.sc.squares.*;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Random;
+import java.awt.Point;
 
 
 public class Board {
@@ -13,43 +16,63 @@ public class Board {
     private LastSquare lastSquare;
     // TODO
     private Players players;
+    Queue<Point> tupleQueue;
 
 
     private Game game;
 
-    public Board(){
-
+    public Board() {
+        tupleQueue = tupleQueueGenerator();
     }
 
-    public int getSize(){
+    public int getSize() {
         return this.size;
     }
 
 
     // Initializes board with given size
     // TODO: Find a way to let the board know all the players? Is this even necessary?
-    private void initBoard(int size, ArrayList PlayerQueue, Game game){
+    private void initBoard(int size, ArrayList PlayerQueue, Game game) {
         this.size = size;
         this.game = game;
-        this.firstSquare = new FirstSquare(this,0); // Debugged by PM
-        this.lastSquare = new LastSquare(this, size-1); // Debugged by PM
+
         Square square;
 
         // Creates a list with all squares in order
-        // Add first square
-        this.squareList.add(0, this.firstSquare);
-        // Add normal, snake and ladder squares
-        for (int i = 1; i < (size - 1); i++){
+
+        // fill the board with normal squares
+        for (int i = 1; i < (size - 1); i++) {
             // predefined ladder and snake places
             /* TODO: (optional)Randomize ladder/snake generation,
+                Tupel übergeben (umkehren für snake)
             */
-            if ( i == 4 || i == 7){ square = new LadderSquare(this, i); }
-            else if (i==9){ square = new SnakeSquare(this, i); }
-            else { square = new NormalSquare(this, i); }
-            this.squareList.add(i, square);
+                square = new NormalSquare(this, i);
+                this.squareList.add(i, square);
         }
+
+        // Add ladders
+        for (int i = 0; i < 2; i++){
+            Point tuple = tupleQueue.remove();
+            int x = (int)tuple.getX();
+            int y = (int)tuple.getY();
+            square = new LadderSquare(this,x,y);
+            this.squareList.add(x,square);
+        }
+        // Add snakes
+        for (int i = 0; i < 2; i++) {
+            Point tuple = tupleQueue.remove();
+            int x = (int) tuple.getX();
+            int y = (int) tuple.getY();
+            square = new SnakeSquare(this, y, x);
+            this.squareList.add(x, square);
+        }
+
+        // Add first square
+        this.firstSquare = new FirstSquare(this, 0);
+        this.squareList.add(0, this.firstSquare);
         // Add last square
-        this.squareList.add(size-1,lastSquare);
+        this.lastSquare = new LastSquare(this, size - 1);
+        this.squareList.add(size - 1, this.lastSquare);
     }
 
 
@@ -59,5 +82,32 @@ public class Board {
 
     // Added by PM
     // TODO: This function should pass winning player (which it gets from lastsquare) to game
-    public void setWinner(Player p){}
+    public void setWinner(Player p) {
+    }
+
+
+    private Queue<Point> tupleQueueGenerator() {
+
+        int result1;
+        int result2;
+        Random r = new Random();
+        int low = 1;
+        int high = 10;
+        Point point = new Point();
+        Queue<Point> myQueue = new LinkedList<>();
+
+        for (int i = 0; i < 4; i++) {
+
+            result1 = r.nextInt(high - low) + low;
+            result2 = result1;
+            while (result2 == result1) {
+                result2 = r.nextInt(high - low) + low;
+            }
+            point.setLocation(result1, result2);
+            myQueue.add(point);
+
+        }
+        return myQueue;
+    }
 }
+
