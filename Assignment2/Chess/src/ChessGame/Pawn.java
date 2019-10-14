5750c6d7c7a69341a261b41612b54ee7f979df02
Wassermalone <pascal.marty98@gmail.com>
2019-10-14 19:21:56 +0200

    Added isValidMove() method to Pawn.

--- Assignment2/Chess/src/ChessGame/Pawn.java
+++ Assignment2/Chess/src/ChessGame/Pawn.java
@@ -3,6 +3,7 @@ package ChessGame;
 import src.ChessGame.Enums.*;
 
 class Pawn extends Piece{
+    private boolean hasMoved = false;
 
     Pawn(Color color) {
         super(color);
@@ -12,8 +13,61 @@ class Pawn extends Piece{
         return true;
     }
 
-    @Override
-    boolean isValidMove(Square current, Square next) {
+    boolean isValidMove(Board board, Square current, Square next) {
+        Square temp;
+        if(getColor() == Color.WHITE){ // only moves up
+            // capture
+            temp = board.getSquare(current.x - 1, current.y - 1);
+            if (temp == next && temp.isOccupied()){
+                return true;
+            }
+            temp = board.getSquare(current.x + 1, current.y - 1);
+            if (temp == next && temp.isOccupied()){
+                return true;
+            }
+            // move
+            temp = board.getSquare(current.x, current.y - 1);
+            if (temp == next){
+                return true;
+            }else if (temp.isOccupied()){
+                return false;
+            }
+            if(!hasMoved){
+                temp = board.getSquare(current.x, current.y - 2);
+                if (temp == next){
+                    return true;
+                }
+            }
+
+        }else if (getColor() == Color.BLACK){ // only moves down
+            // capture
+            temp = board.getSquare(current.x - 1, current.y + 1);
+            if (temp == next && temp.isOccupied()){
+                return true;
+            }
+            temp = board.getSquare(current.x + 1, current.y + 1);
+            if (temp == next && temp.isOccupied()){
+                return true;
+            }
+            // move
+            temp = board.getSquare(current.x, current.y + 1);
+            if (temp == next){
+                return true;
+            }else if (temp.isOccupied()){
+                return false;
+            }
+            if(!hasMoved){
+                temp = board.getSquare(current.x, current.y + 2);
+                if (temp == next){
+                    return true;
+                }
+            }
+
+        }
+
+        if(!hasMoved){
+            hasMoved = true;
+        }
         return false;
     }
 }
