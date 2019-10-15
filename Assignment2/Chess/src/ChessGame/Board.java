50923e724f1d1f5ee09af491233349da76db09a5
Tannebaum007 <45392509+Tannebaum007@users.noreply.github.com>
2019-10-15 16:19:02 +0200

    changed placement of the pieces on turn 0

--- Assignment2/Chess/src/ChessGame/Board.java
+++ Assignment2/Chess/src/ChessGame/Board.java
@@ -22,29 +22,29 @@ public class Board {
 
     private void setPieces() {
         squares[0][0].addPiece(new Rook(Color.BLACK));
-        squares[0][7].addPiece(new Rook(Color.WHITE));
-        squares[7][0].addPiece(new Rook(Color.BLACK));
+        squares[7][0].addPiece(new Rook(Color.WHITE));
+        squares[0][7].addPiece(new Rook(Color.BLACK));
         squares[7][7].addPiece(new Rook(Color.WHITE));
 
-        squares[1][0].addPiece(new Knight(Color.BLACK));
-        squares[1][7].addPiece(new Knight(Color.WHITE));
-        squares[6][0].addPiece(new Knight(Color.BLACK));
-        squares[6][7].addPiece(new Knight(Color.WHITE));
+        squares[0][1].addPiece(new Knight(Color.BLACK));
+        squares[7][1].addPiece(new Knight(Color.WHITE));
+        squares[0][6].addPiece(new Knight(Color.BLACK));
+        squares[7][6].addPiece(new Knight(Color.WHITE));
 
-        squares[2][0].addPiece(new Bishop(Color.BLACK));
-        squares[2][7].addPiece(new Bishop(Color.WHITE));
-        squares[5][0].addPiece(new Bishop(Color.BLACK));
-        squares[5][7].addPiece(new Bishop(Color.WHITE));
+        squares[0][2].addPiece(new Bishop(Color.BLACK));
+        squares[7][2].addPiece(new Bishop(Color.WHITE));
+        squares[0][5].addPiece(new Bishop(Color.BLACK));
+        squares[7][5].addPiece(new Bishop(Color.WHITE));
 
-        squares[3][0].addPiece(new Queen(Color.BLACK));
-        squares[3][7].addPiece(new Queen(Color.WHITE));
+        squares[0][3].addPiece(new Queen(Color.BLACK));
+        squares[7][3].addPiece(new Queen(Color.WHITE));
 
-        squares[4][0].addPiece(new King(Color.BLACK));
-        squares[4][7].addPiece(new King(Color.WHITE));
+        squares[0][4].addPiece(new King(Color.BLACK));
+        squares[7][4].addPiece(new King(Color.WHITE));
 
         for (int i = 0; i < 8; i++) {
-            squares[i][1].addPiece(new Pawn(Color.BLACK));
-            squares[i][6].addPiece(new Pawn(Color.WHITE));
+            squares[1][i].addPiece(new Pawn(Color.BLACK));
+            squares[6][i].addPiece(new Pawn(Color.WHITE));
         }
     }
 
